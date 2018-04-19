ea948ab9f6b3b6cc9c23c68b94911db7ea6b0485
Phil Wade <phil@philwade.org>
2018-04-19 14:04:09 -0400

    just kidding really upgrade

--- timeout.rb
+++ timeout.rb
@@ -1,7 +1,7 @@
 class Timeout < Formula
   desc "A simple tool for blocking websites in the name of productivity"
   homepage "http://timeout.philwade.org/"
-  url "https://github.com/philwade/timeout/releases/download/v1.0.0/timeout-v0.2.0-osx.tar.gz"
+  url "https://github.com/philwade/timeout/releases/download/v1.0.0/timeout-v1.0.0-osx.tar.gz"
   sha256 "d590b588e26d0ea8b3c572c87b5ba8ae472acaadc48ea0cf2b3c3316be2f9400"
 
   bottle :unneeded
